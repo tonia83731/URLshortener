e73f1880f83de585e8425a5ecd4f62849f4fd9f6
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-10-15 11:50:23 +0800

    feat: finished url shortener in app.js

--- app.js
+++ app.js
@@ -36,17 +36,34 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/output", (req, res) => {
- res.render("output");
-})
-app.post("/generate", (req, res) => {
+// app.get("/output", (req, res) => {
+//  res.render("output");
+// })
+app.post("/", (req, res) => {
   const url = req.body.url
-  const shortenUrl = URLgenerator()
-  
-  // URL.create({
-  //   url, shortenUrl
-  // })
+  if(!url) return res.redirect('/')
+  const shortenUrl = URLgenerator(6)
+  // console.log(req.headers)
+
+  URL.findOne({url: url})
+    .then(data => data ? data : URL.create({shortenUrl, url}))
+    .then(data => res.render("index", {
+      origin: req.headers.origin, 
+      shortenUrl: data.shortenUrl
+    }))
+    .catch(error => console.log(error))
 });
+app.get("/:shorten", (req, res) => {
+  const shortenUrl = req.params.shorten
+  URL.findOne({ shortenUrl })
+    .then(data => {
+      if(!data){
+        return res.redirect("/");
+      }
+      res.redirect(data.url)
+    })
+    .catch(error => console.log(error))
+})
 
 app.listen(3000, () => {
   console.log("App is running on http://localhost:3000");
--- url_generator.js
+++ url_generator.js
@@ -1,19 +1,24 @@
+const lowercaseLetter = 'abcdefghijklmnopqrstuvwxyz'
+const uppercaseLetter = lowercaseLetter.toUpperCase()
+const number = '0123456789'
+const base_collection = lowercaseLetter + uppercaseLetter + number
+const max = 61
+const min = 0
+
 function sample(arr){
-  let index = Math.floor(Math.random() * arr.length);
+  let index = Math.floor(Math.random() * (max - min + 1) + min);
   return arr[index]
 }
 
 
-function URLgenerator(){
-  const lowercaseLetter = 'abcdefghijklmnopqrstuvwxyz'
-  const uppercaseLetter = lowercaseLetter.toUpperCase()
-  const number = '0123456789'
+function URLgenerator(length){
+  
 
   let collection = lowercaseLetter + uppercaseLetter + number
   let collectionArr = collection.split('')
   // console.log(collectionArr);
   let password = ''
-  for(let i=0; i<6; i++){
+  for(let i=0; i < length; i++){
     password += sample(collectionArr)
   }
   // console.log(password)
